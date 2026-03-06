a8faaacf7ea8789e902871b8ebfa3513447323b4
Midhun <midhun236899@gmail.com>
2026-03-06 06:22:02 +0530

    Integrate Qin input and enhance multiplication logic
    Updated the mul module to incorporate Qin input and modified internal logic for handling multiplication.

--- mul.sv
+++ mul.sv
@@ -1,37 +1,30 @@
 module mul(
     input logic [4:0] M,
-    input logic [4:0] Q,
+    input logic [4:0] Qin,
     input logic clk,
     input logic rst,
     output logic [9:0] R
 );
-    logic Qp=1'b0;
     logic [2:0] pc;
     logic [4:0] A;
-    initial begin
-    logic Qp=1'b0;
-    logic [4:0] A=5'b00000;
-    logic [2:0] pc=3'b000;
-    end
+    logic [4:0] Q;
+    logic Qp;
+    logic done;
+    wire [4:0] A_next=({Q[0],Qp}==2'b01) ? (A+M):
+                      ({Q[0],Qp}==2'b10) ? (A+~M+1): A;
+    assign R={A,Q};
     always_ff @(posedge clk or posedge rst ) begin
-            if(clk) begin
-                pc<=pc+1;
-                if(pc<6) begin
-                    case({Qp,Q[0]})
-                    2'b01: A<= A+M;
-                    2'b10: A<= A+~M+1;
-                    default: ;
-                    endcase
-                    Qp<=Q[0];
-                    A<=A>>>1;
-                end
-                else begin
-                    R<={A,Q};
-                end            
-            end
-            else if(rst) begin
+            if(rst) begin
                 A<=5'b00000;
-                pc<=0;
+                Q<=Qin;
+                Qp<=1'b0;
+                pc<=3'b000;
+                done<=1'b0;
             end
+            else if(pc<5) begin
+                    A<={A_next[4],A_next[4:1]};
+                    Q<={A_next[0],Q[4:1]};
+                    pc<=pc+1;
+            end           
     end
 endmodule
